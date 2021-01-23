e3aa85428f0645c886c4adc12029e0f00b89508f
yli-hub <li000713@algonquinlive.com>
2021-01-23 18:26:33 -0500

    lab2

--- app/src/main/java/com/cst2335/li000713/MainActivity.java
+++ app/src/main/java/com/cst2335/li000713/MainActivity.java
@@ -3,12 +3,27 @@ package com.cst2335.li000713;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
+        TextView myText=findViewById(R.id.text);
+        EditText myEdit = findViewById(R.id.edit);
+        Button btn = findViewById(R.id.button1);
+        btn.setOnClickListener( OnClickListener obj );
+
+
     }
 }
