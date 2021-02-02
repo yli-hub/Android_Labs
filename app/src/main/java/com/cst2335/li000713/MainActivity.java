e7aa3910b1d4c95cd531ef1897c1504fe6399886
yli-hub <li000713@algonquinlive.com>
2021-02-01 23:37:41 -0500

    Lab2 Step2

--- app/src/main/java/com/cst2335/li000713/MainActivity.java
+++ app/src/main/java/com/cst2335/li000713/MainActivity.java
@@ -22,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main_relative);
-        setContentView(R.layout.activity_main_linear);
-        //setContentView(R.layout.activity_main_grid);
+        //setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
         //setContentView(R.layout.activity_main_relative);
         //TextView myText=findViewById(R.id.text);
         //EditText myEdit = findViewById(R.id.edit);
