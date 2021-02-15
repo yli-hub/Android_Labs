d0ac270e5920b673ceeaf6bae0666c3ae2cd088c
Ying Li <77427357+yli-hub@users.noreply.github.com>
2021-02-15 00:00:16 -0500

    layout

--- app/src/main/java/com/cst2335/li000713/ChatRoomActivity.java
+++ app/src/main/java/com/cst2335/li000713/ChatRoomActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -28,10 +30,44 @@ public class ChatRoomActivity<MyListAdapter> extends AppCompatActivity {
 
         setContentView(R.layout.activity_chat_room);
         ListView chatlist = (ListView) findViewById(R.id.crlv);
+        EditText etext = findViewById(R.id.type);
+        Button sendBtn = findViewById(R.id.sendbtn);
+        sendBtn.setOnClickListener( click ->{
+            Message smsg = new Message(etext.toString(), R.drawable.row_send,0);
+            element.add(smsg);
+            myAdapter;
+        });
+        Button rcvBtn = findViewById(R.id.rcvbtn);
+        rcvBtn.setOnClickListener( click -> {
+            Message rmsg = new Message(etext.toString(), R.drawable.row_receive, 1);
+                    element.add(rmsg);
+        });
         myAdapter = (MyListAdapter) new ChatRoomActivity.MyListAdapter();
         chatlist.setAdapter((ListAdapter) myAdapter);
     }
-    private ArrayList<String> element = new ArrayList<>();
+    class Message {
+        private String text;
+        private int img;
+        private int type;
+
+        public Message (String text ,int img ,int type){
+            this.text = text;
+            this.img = img;
+            this.type = type;
+        }
+        public String getText(){
+            return text;
+        }
+        public int getImg(){
+            return img;
+        }
+        public int getType(){
+            return type;
+        }
+
+
+    }
+    private ArrayList<Message> element = new ArrayList<>();
     class MyListAdapter extends BaseAdapter{
 
 
@@ -52,10 +88,12 @@ public class ChatRoomActivity<MyListAdapter> extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(int row, View convertView, ViewGroup parent) {
             LayoutInflater inflater = getLayoutInflater();
             ViewGroup viewGroup = null;
-            View thisRow = inflater.inflate(R.layout.row_layout, viewGroup, false);
+            View thisRow = inflater.inflate(R.layout.row_send_layout, viewGroup, false);
+            TextView thisRowText= thisRow.findViewById(R.id.tx_send);
+            thisRowText.setText(getItem(row));
             return thisRow;
         }
     }
