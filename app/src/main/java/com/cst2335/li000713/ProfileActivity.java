25b28ffc225e010368e31f4c5565307df8b803ba
Ying Li <77427357+yli-hub@users.noreply.github.com>
2021-03-30 23:50:16 -0400

    i c

--- app/src/main/java/com/cst2335/li000713/ProfileActivity.java
+++ app/src/main/java/com/cst2335/li000713/ProfileActivity.java
@@ -1,21 +1,16 @@
 package com.cst2335.li000713;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-
-
-public class ProfileActivity<click> extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
@@ -47,7 +42,8 @@ public class ProfileActivity<click> extends AppCompatActivity {
         chatbtn.setOnClickListener(click -> {
             startActivity(goToChatrm);
         });
-    }
+
+         }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
